7086b46ea3056303d5f730900818d7a26e6be395
hemant159 <emeehemant159@gmail.com>
2025-01-22 17:28:46 +0530

    group creating successfully

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -3,9 +3,10 @@ import { ErrorHandler } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
 import { emitEvent } from "../utils/features.js";
 import { ALERT, REFETCH_CHATS } from "../constants/events.js";
+import { getOtherMember } from "../lib/helper.js";
 
 const newGroup = TryCatch(async (req, res, next) => {
-    const { name, members} = req.body;
+    const { name, members } = req.body;
 
     if(members.length < 2) {
         return next(
@@ -31,4 +32,36 @@ const newGroup = TryCatch(async (req, res, next) => {
     })
 });
 
-export { newGroup };
+const getMyChats = TryCatch(async (req, res, next) => {
+    const chats = await Chat.find({ members: req.user }).populate(
+        "members",
+        "name avatar"
+    )
+    
+    const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+
+        const otherMember = getOtherMember(members, req.user);
+
+        return {
+            _id,
+            groupChat,
+            avatar: groupChat 
+            ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+            : [otherMember.avatar.url],
+            name: groupChat ? name : otherMember.name,
+            members: members.reduce((prev, curr) => {
+                if(curr._id.toString() !== req.user) {
+                    prev.push(curr._id);
+                }
+                return prev;
+            }, []),
+        };
+    })
+
+    return res.status(200).json({
+        sucess: true,
+        chats: transformedChats 
+    })
+});
+
+export { newGroup, getMyChats };
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newGroup } from "../controllers/chat.js";
+import { newGroup, getMyChats } from "../controllers/chat.js";
 
 const app = express.Router();
 
 app.use(isAuthenticated);
 
 app.post("/newGroup", newGroup);
+app.get("/my", getMyChats);
 
 
 export default app;
